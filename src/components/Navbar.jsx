b8ec3e93ec1e45ca0691099b69369c5132321c67
Mehmet Nadi <mehmetnadi@Mehmets-MBP.lan>
2020-12-02 16:26:48 -0800

    search page searc container design is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import HomeIcon from "@material-ui/icons/Home";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import IconButton from "@material-ui/core/IconButton";
+import { Link } from "react-router-dom";
 
 /*
 Main Colors:
@@ -19,15 +20,19 @@ function Navbar() {
   return (
     <div className="Navbar">
       <div className="homeBtn-box">
-        <h1 className="title">
-          <HomeIcon fontSize="large" style={{ color: "#ffd31d" }} />
-          Hungry!¡!¡
-        </h1>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1 className="title">
+            <HomeIcon fontSize="large" style={{ color: "#ffd31d" }} />
+            Hungry!¡!¡
+          </h1>
+        </Link>
       </div>
       <div className="heartBtn-box">
-        <IconButton aria-label="delete">
-          <FavoriteIcon fontSize="large" style={{ color: "#ffd31d" }} />
-        </IconButton>
+        <Link to="/FavoritePage" style={{ textDecoration: "none" }}>
+          <IconButton aria-label="delete">
+            <FavoriteIcon fontSize="large" style={{ color: "#ffd31d" }} />
+          </IconButton>
+        </Link>
       </div>
     </div>
   );
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,10 +1,61 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+
 import "./SearchPage.css";
 
 function SearchPage() {
   return (
     <div className="SearchPage">
-      <h1>SearchPage</h1>
+      <div className="search-box-container">
+        <Paper
+          component="form"
+          style={{
+            backgroundColor: "white",
+            borderRadius: "30px",
+            boxShadow:
+              "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            borderColor: "#f6cd61",
+            padding: "10px 20px",
+          }}
+        >
+          <InputBase
+            placeholder="Delicious Recepies..."
+            style={{ color: "#ec0101", width: "fit-content" }}
+          />
+          <IconButton
+            type="submit"
+            aria-label="search"
+            style={{
+              color: "#ffd31d",
+              marginLeft: "10px",
+            }}
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </div>
+      <div className="search-result-wrapper">
+        <div>
+          <h1>result</h1>
+        </div>
+        <div>
+          <h1>result</h1>
+        </div>
+        <div>
+          <h1>result</h1>
+        </div>
+        <div>
+          <h1>result</h1>
+        </div>
+        <div>
+          <h1>result</h1>
+        </div>
+      </div>
     </div>
   );
 }
