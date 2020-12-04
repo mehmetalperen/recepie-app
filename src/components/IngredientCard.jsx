3a5764c5da377130877616efef8c810176dff288
Mehmet Nadi <mehmetnadi@Mehmets-MBP.lan>
2020-12-03 18:43:46 -0800

    need to fecth the data and need to work on functionality. also did not do anything about similar recepie card

--- src/components/IngredientCard.jsx
+++ src/components/IngredientCard.jsx
@@ -3,9 +3,10 @@ import "./IngredientCard.css";
 import IngredientItem from "./IngredientItem";
 //import IconButton from "@material-ui/core/IconButton";
 
-function IngredientCard() {
+function IngredientCard(s) {
   return (
     <div className="IngredientCard">
+      <h3 className="total-ingredient">Total ingredienst: ##</h3>
       <IngredientItem />
       <IngredientItem />
       {/*make it scrollable if there is more than five ingredienst*/}
--- src/components/InstructionCard.jsx
+++ src/components/InstructionCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./InstructionCard.css";
+import RecepieSteps from "./RecepieSteps";
 //import IconButton from "@material-ui/core/IconButton";
 
 function InstructionCard() {
   return (
     <div className="InstructionCard">
-      <h1>InstructionCard</h1>
+      <h1 className="total-steps">Total steps: ##</h1>
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
+      <RecepieSteps />
     </div>
   );
 }
